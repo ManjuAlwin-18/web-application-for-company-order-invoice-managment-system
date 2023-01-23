63ed984f0dd518d74c5f4e5c419e01d45a5dd2ef
Manju Davis <manjudavis18@gmail.com>
2023-01-24 10:47:57 +1300

    initial Commits

---
+++ theme/includes/navigation.php
@@ -0,0 +1,26 @@
+<div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+        <div class="nano">
+            <div class="nano-content">
+                <ul>
+                    <div class="logo"><a href="index.html">
+                            <!-- <img src="images/logo.png" alt="" /> --><span>Focus</span></a></div>
+                    
+                    <li><a href="company.php"><i class="ti-view-list-alt"></i> Company </a></li>
+                    <li><a href="projects.php"><i class="ti-view-list-alt"></i> Projects </a></li>
+                    <li><a href="items.php"><i class="ti-view-list-alt"></i> Items </a></li>
+                    <li><a href="category.php"><i class="ti-view-list-alt"></i> Category </a></li>
+                    <li><a href="products.php"><i class="ti-view-list-alt"></i> Products </a></li>
+                    <li><a href="quotation.php"><i class="ti-view-list-alt"></i> Quotation </a></li>
+                    <li><a href="invoice.php"><i class="ti-view-list-alt"></i> Invoice </a></li>
+                    
+                    
+                    
+                    <li><a><i class="ti-close"></i> Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+
+
