206149a422643b856b3cd3886d5c2b805061aafe
Manju Davis <manjudavis18@gmail.com>
2023-01-24 13:21:21 +1300

    modified category page, index page

--- theme/pages/index.php
+++ theme/pages/index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <!--Head Includes starts-------------------------------------------------------------------------------------->
-<?PHP include('includes/head.php');?>
+<?PHP include('../includes/head.php');?>
 <!--Head Includes Ends---------------------------------------------------------------------------------------->
 
 <body>
 
     <!--Navigation Includes Starts----------------------------------------------------------------------------->
-    <?PHP include('includes/navigation.php');?>
+    <?PHP include('../includes/navigation.php');?>
     <!--Navigation Includes Ends------------------------------------------------------------------------------->
     
     <!--Header Includes Starts--------------------------------------------------------------------------------->
-    <?PHP include('includes/header.php');?>
+    <?PHP include('../includes/header.php');?>
     <!--Navigation Includes Ends-------------------------------------------------------------------------------> 
 
     <div class="content-wrap">
@@ -735,7 +735,7 @@
                         <!-- /# column -->
                     </div>
                     <!--Navigation Includes Starts ---------------------------------------------------------->
-                    <?PHP include('includes/footer.php');?>
+                    <?PHP include('../includes/footer.php');?>
                     <!--Navigation Includes Ends ---------------------------------------------------------->
                 </section>
             </div>
@@ -743,33 +743,33 @@
     </div>
 
     <!-- jquery vendor -->
-    <script src="js/lib/jquery.min.js"></script>
-    <script src="js/lib/jquery.nanoscroller.min.js"></script>
+    <script src="../js/lib/jquery.min.js"></script>
+    <script src="../js/lib/jquery.nanoscroller.min.js"></script>
     <!-- nano scroller -->
-    <script src="js/lib/menubar/sidebar.js"></script>
-    <script src="js/lib/preloader/pace.min.js"></script>
+    <script src="../js/lib/menubar/sidebar.js"></script>
+    <script src="../js/lib/preloader/pace.min.js"></script>
     <!-- sidebar -->
 
-    <script src="js/lib/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
+    <script src="../js/lib/bootstrap.min.js"></script>
+    <script src="../js/scripts.js"></script>
     <!-- bootstrap -->
 
-    <script src="js/lib/calendar-2/moment.latest.min.js"></script>
-    <script src="js/lib/calendar-2/pignose.calendar.min.js"></script>
-    <script src="js/lib/calendar-2/pignose.init.js"></script>
+    <script src="../js/lib/calendar-2/moment.latest.min.js"></script>
+    <script src="../js/lib/calendar-2/pignose.calendar.min.js"></script>
+    <script src="../js/lib/calendar-2/pignose.init.js"></script>
 
 
-    <script src="js/lib/weather/jquery.simpleWeather.min.js"></script>
-    <script src="js/lib/weather/weather-init.js"></script>
-    <script src="js/lib/circle-progress/circle-progress.min.js"></script>
-    <script src="js/lib/circle-progress/circle-progress-init.js"></script>
-    <script src="js/lib/chartist/chartist.min.js"></script>
-    <script src="js/lib/sparklinechart/jquery.sparkline.min.js"></script>
-    <script src="js/lib/sparklinechart/sparkline.init.js"></script>
-    <script src="js/lib/owl-carousel/owl.carousel.min.js"></script>
-    <script src="js/lib/owl-carousel/owl.carousel-init.js"></script>
+    <script src="../js/lib/weather/jquery.simpleWeather.min.js"></script>
+    <script src="../js/lib/weather/weather-init.js"></script>
+    <script src="../js/lib/circle-progress/circle-progress.min.js"></script>
+    <script src="../js/lib/circle-progress/circle-progress-init.js"></script>
+    <script src="../js/lib/chartist/chartist.min.js"></script>
+    <script src="../js/lib/sparklinechart/jquery.sparkline.min.js"></script>
+    <script src="../js/lib/sparklinechart/sparkline.init.js"></script>
+    <script src="../js/lib/owl-carousel/owl.carousel.min.js"></script>
+    <script src="../js/lib/owl-carousel/owl.carousel-init.js"></script>
     <!-- scripit init-->
-    <script src="js/dashboard2.js"></script>
+    <script src="../js/dashboard2.js"></script>
 </body>
 
 </html>
