f43bd25b3214f8b9bf54982f6128c9f2574961af
Manju Davis <manjudavis18@gmail.com>
2023-01-24 17:15:44 +1300

    modified navigation menu, Company page, Invoice, Items, Pro

--- theme/includes/navigation.php
+++ theme/includes/navigation.php
@@ -5,13 +5,13 @@
                     <div class="logo"><a href="index.html">
                             <!-- <img src="images/logo.png" alt="" /> --><span>Focus</span></a></div>
                     
-                    <li><a href="pages/company.php"><i class="ti-view-list-alt"></i> Company </a></li>
-                    <li><a href="pages/projects.php"><i class="ti-view-list-alt"></i> Projects </a></li>
-                    <li><a href="pages/items.php"><i class="ti-view-list-alt"></i> Items </a></li>
-                    <li><a href="pages/category.php"><i class="ti-view-list-alt"></i> Category </a></li>
-                    <li><a href="pages/products.php"><i class="ti-view-list-alt"></i> Products </a></li>
-                    <li><a href="pages/quotation.php"><i class="ti-view-list-alt"></i> Quotation </a></li>
-                    <li><a href="pagesinvoice.php"><i class="ti-view-list-alt"></i> Invoice </a></li>
+                    <li><a href="company.php"><i class="ti-view-list-alt"></i> Company </a></li>
+                    <li><a href="projects.php"><i class="ti-view-list-alt"></i> Projects </a></li>
+                    <li><a href="items.php"><i class="ti-view-list-alt"></i> Items </a></li>
+                    <li><a href="category.php"><i class="ti-view-list-alt"></i> Category </a></li>
+                    <li><a href="products.php"><i class="ti-view-list-alt"></i> Products </a></li>
+                    <li><a href="quotation.php"><i class="ti-view-list-alt"></i> Quotation </a></li>
+                    <li><a href="invoice.php"><i class="ti-view-list-alt"></i> Invoice </a></li>
                     
                     
                     
