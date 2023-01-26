54d9f54c13985fefdaeb08e0cd0e4c8470e0a544
ManjuAlwin-18 <123202668+ManjuAlwin-18@users.noreply.github.com>
2023-01-26 18:01:01 +1300

    Delete theme directory

--- theme/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-                        <div class="col-lg-12">
-                            <div class="footer">
-                                <p>2018 © Admin Board. - <a href="#">example.com</a></p>
-                            </div>
-                        </div>
-                    </div>
